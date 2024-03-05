69c2d90c06f2aee2a1b832e01e9865b766fb2721
Joseph Stewart <josephlikescode@gmail.com>
2024-03-04 20:03:41 -0800

    Remove console log.

--- backend/listOrders.ts
+++ backend/listOrders.ts
@@ -3,7 +3,6 @@ import { listOrders } from './webflowClient';
 
 async function main() {
 	const orders = await listOrders();
-	console.log(JSON.stringify(orders, null, 4));
 	await addOrderData(orders);
 }
 
